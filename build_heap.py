b71560c6d4b4b73d65330df662f922697e9d5d25
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-03-14 21:37:12 +0200

    test

--- build_heap.py
+++ build_heap.py
@@ -1,77 +1,62 @@
-# python3
-
+import sys
 
 def build_heap(data):
     n = len(data)
     swaps = []
-    for i in range(n // 2, -1, -1):
-        swaps = sift_down(data, i, swaps)
+    for i in range(n//2, -1, -1):
+        swaps += sift_down(data, i, n)
     return swaps
 
-def sift_down(data, i, swaps):
+def sift_down(data, i, n):
+    swaps = []
+    max_idx = i
+    l = 2*i + 1
+    if l < n and data[l] > data[max_idx]:
+        max_idx = l
+    r = 2*i + 2
+    if r < n and data[r] > data[max_idx]:
+        max_idx = r
+    if i != max_idx:
+        swaps.append((i, max_idx))
+        data[i], data[max_idx] = data[max_idx], data[i]
+        swaps += sift_down(data, max_idx, n)
+    return swaps
+
+
+def heap_sort(data):
+    swaps = []
     n = len(data)
-    min_idx = i
-    l = 2 * i + 1
-    if l < n and data[l] < data[min_idx]:
-        min_idx = l
-    r = 2 * i + 2
-    if r < n and data[r] < data[min_idx]:
-        min_idx = r
-    if i != min_idx:
-        data[i], data[min_idx] = data[min_idx], data[i]
-        swaps.append((i, min_idx))
-        swaps = sift_down(data, min_idx, swaps)
-    return swaps
 
-    
+    # Build the heap in O(n) time complexity
+    build_heap(data)
+
+    # Perform heap sort by removing the root element and re-heapifying
+    for i in range(n-1, 0, -1):
+        swaps.append((0, i))
+        data[0], data[i] = data[i], data[0]
+        sift_down(data[:i], 0, swaps)
+
+    return swaps
 
 def main():
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    # Read input from either keyboard or file
+    if len(sys.argv) == 1 or sys.argv[1] == 'I':
+        n = int(input())
+        data = list(map(int, input().split()))
+    else:
+        filename = sys.argv[1]
+        with open(filename, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
 
-    # checks if length of data is the same as the said length
+    # Check if the length of data is the same as the given length
     assert len(data) == n
 
-    # add input for I or F
-    sort_type = input("Enter sort type (I for increasing, F for decreasing): ").strip()
-    assert sort_type in ["I", "F"]
-
-    # convert data to min-heap using O(n) swaps
-    swaps = build_heap(data)
-
-    # output how many swaps were made (less than 4n)
+    # Perform heap sort and count the number of swaps
+    swaps = heap_sort(data)
     print(len(swaps))
-
-    # output all swaps
     for i, j in swaps:
         print(i, j)
-
-    # output the sorted data in increasing or decreasing order
-    if sort_type == "I":
-        sorted_data = heap_sort(data)
-    else:
-        # read data from file
-        filename = input("Enter filename: ").strip()
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-            # convert each line to an integer and append to a list
-            sorted_data = [int(x) for x in lines]
-        sorted_data = heap_sort(sorted_data, reverse=True)
-    print(*sorted_data)
-
-
-
-def heap_sort(data, reverse=False):
-    n = len(data)
-    for i in range(n - 1, 0, -1):
-        data[0], data[i] = data[i], data[0]
-        sift_down(data, 0, i)
-    if reverse:
-        data.reverse()
-    return data
-
-
 
 if __name__ == "__main__":
     main()
