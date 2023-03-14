d1fe2739f32c12dd906e63a6cd2b9961f63bfa63
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-03-14 21:23:25 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,40 +2,68 @@
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(n // 2, -1, -1):
+        swaps = sift_down(data, i, swaps)
     return swaps
+
+def sift_down(data, i, swaps):
+    n = len(data)
+    min_idx = i
+    l = 2 * i + 1
+    if l < n and data[l] < data[min_idx]:
+        min_idx = l
+    r = 2 * i + 2
+    if r < n and data[r] < data[min_idx]:
+        min_idx = r
+    if i != min_idx:
+        data[i], data[min_idx] = data[min_idx], data[i]
+        swaps.append((i, min_idx))
+        swaps = sift_down(data, min_idx, swaps)
+    return swaps
+
+    
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-FSDF
     # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
+    # checks if length of data is the same as the said length
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
+    # add input for I or F
+    sort_type = input("Enter sort type (I for increasing, F for decreasing): ")
+    assert sort_type in ["I", "F"]
+
+    # convert data to min-heap using O(n) swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
+    # output how many swaps were made (less than 4n)
+    print(len(swaps))
 
     # output all swaps
-    print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
+    # output the sorted data in increasing or decreasing order
+    if sort_type == "I":
+        sorted_data = heap_sort(data)
+    else:
+        sorted_data = heap_sort(data, reverse=True)
+    print(*sorted_data)
+
+def heap_sort(data, reverse=False):
+    n = len(data)
+    for i in range(n - 1, 0, -1):
+        data[0], data[i] = data[i], data[0]
+        sift_down(data, 0, i)
+    if reverse:
+        data.reverse()
+    return data
+
 
 
 if __name__ == "__main__":
