40754abe9fbd9a8e3a474c9d501137924466769a
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-03-15 22:13:04 +0200

    hehe

--- build_heap.py
+++ build_heap.py
@@ -2,56 +2,41 @@ def build_heap(data):
     swaps = []
     n = len(data)
 
-    # Build the heap bottom-up using the Floyd's algorithm
-    for i in range(n // 2 - 1, -1, -1):
+    # Starting from the last non-leaf node and going up to the root
+    # perform sift down operation to convert the array into a min-heap
+    for i in range(n // 2, -1, -1):
         min_index = i
-
         left_child = 2 * i + 1
+        right_child = 2 * i + 2
+
         if left_child < n and data[left_child] < data[min_index]:
             min_index = left_child
 
-        right_child = 2 * i + 2
         if right_child < n and data[right_child] < data[min_index]:
             min_index = right_child
 
         if i != min_index:
-            swaps.append((i, min_index))
+            # swap the current element with the minimum of its children
             data[i], data[min_index] = data[min_index], data[i]
+            swaps.append((i, min_index))
+            swaps += build_heap(data[min_index:])
 
     return swaps
 
 
-def sift_down(i, data, swaps):
-    n = len(data)
-    while 2*i+1 < n:
-        j = 2*i+1
-        if j+1 < n and data[j+1] < data[j]:
-            j = j+1
-        if data[i] > data[j]:
-            swaps.append((i, j))
-            data[i], data[j] = data[j], data[i]
-            i = j
-        else:
-            break
-
 def main():
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if length of data is the same as the said length
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # output how many swaps were made
     print(len(swaps))
-
-    # output all swaps
     for i, j in swaps:
         print(i, j)
 
+
 if __name__ == "__main__":
     main()
+
